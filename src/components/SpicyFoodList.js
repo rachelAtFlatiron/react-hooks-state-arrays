663716167a0a185fc8f21a1caf8e6793bda865b0
Rachel Katz <rachel.katz@flatironschool.com>
2022-03-31 12:39:41 -0400

    setFoods([...foods]) and my solution

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,17 +6,38 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    
+    const newFoods = [...foods, newFood]
+    setFoods(newFoods)
+  }
+  function handleLiClick(id){
+    //const newFoods = foods.filter(food => food.id !== id)
+    let foodIndex = foods.findIndex(el => {return el.id === id});
+
+    //MUST MAKE THIS A NEW ARRAY - CANNOT UPDATE PREVIOUS STATE BY 
+    //DIRECTLY MANIPULATING IT 
+    let newFoods = [...foods];
+    
+    newFoods[foodIndex].heatLevel += 1;
+    
+    setFoods(newFoods); //setFoods([...newFoods])
   }
 
+  function handleRemoveClick(id){
+    const newFoods = foods.filter(food => food.id !== id)
+    setFoods(newFoods);
+  }
+
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li onClick={() => handleLiClick(food.id)} key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-
+  console.log(foods);
   return (
     <div>
+      
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
